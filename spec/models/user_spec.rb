435e66957fe2ea43979576723ab7b595aa596a15
Yusuke Tachibana <yusuke3627@gmail.com>
2019-01-26 09:40:17 +0900

    code fixed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with email, password"
+  it "is invalid without email"
+  it "is invalid without password"
+  it "is invalid without password"
 end
